770fb7afd2c4b7b93cc97812c03dbf8ee617d12d
anhducle <anhducle@coccoc.com>
2019-01-12 12:52:15 +0700

    add reverse_dns_lookup, refactor

--- ping.cpp
+++ ping.cpp
@@ -23,6 +23,8 @@
 
 using namespace std;
 
+string host_string;
+
 uint16_t checksum(uint16_t *addr, uint32_t len) {
 	uint16_t answer = 0;
 	uint32_t sum = 0;
@@ -54,9 +56,7 @@ string get_ip_string(uint32_t num) {
 	return ip_string;
 }
 
-int ping(const string &target) {
-	string host_string = target;
-
+sockaddr_in dns_lookup(const string &target) {
 	sockaddr_in to;
 	to.sin_family = AF_INET;
 	to.sin_addr.s_addr = inet_addr(target.c_str());
@@ -64,29 +64,37 @@ int ping(const string &target) {
 		hostent *hp = gethostbyname(target.c_str());
 		if (!hp) {
 			cerr << "unknown host "<< target << endl;
-			return -1;
+		} else {
+			memcpy(&to.sin_addr, hp->h_addr, hp->h_length);
 		}
-		memcpy(&to.sin_addr, hp->h_addr, hp->h_length);
-		host_string += " (" + get_ip_string(to.sin_addr.s_addr) + ")";
 	}
+	return to;
+}
 
-	static bool first_time = true;
-	if (first_time) {
-		printf("PING %s with %d bytes of data\n", host_string.c_str(), ICMP_DATA_LENGTH);
-		first_time = false;
+string reverse_dns_lookup(const sockaddr_in &addr) {
+	socklen_t len;
+	char buf[100], *ret_buf;
+	len = sizeof(struct sockaddr_in);
+	if (getnameinfo((sockaddr*)&addr, sizeof(sockaddr_in), buf, sizeof(buf), NULL, 0, NI_NAMEREQD)) {
+		printf("Could not resolve reverse lookup of hostname\n");
+		return "";
 	}
+	return buf;
+}
 
-	int socket_id = -1;
-	if ((socket_id = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)) < 0) {
+int create_socket() {
+	int socket_id = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+	if (socket_id < 0) {
 		cerr << "please run as superuser" << endl;
 		return -1;
 	}
+	return socket_id;
+}
 
-	static n_short sequence_number = 0;
-	sequence_number++;
-
+int send_icmp_packet(const sockaddr_in &to, int socket_id, int sequence_number) {
 	int outpacket_length = ICMP_DATA_LENGTH + ICMP_MINLEN;
 	static char *out_buffer = new char[outpacket_length];
+
 	icmp *icmp_packet = (icmp*)out_buffer;
 	icmp_packet->icmp_type = ICMP_ECHO;
 	icmp_packet->icmp_seq = sequence_number;
@@ -95,16 +103,17 @@ int ping(const string &target) {
 	icmp_packet->icmp_id = getpid();
 	icmp_packet->icmp_cksum = checksum((uint16_t*)icmp_packet, outpacket_length);
 
-	clock_t start_time = clock();
-
 	int num_char_sent = sendto(socket_id, out_buffer, outpacket_length, 0, (sockaddr*)&to, (socklen_t)sizeof(sockaddr_in));
-	delete icmp_packet;
 
 	if (num_char_sent < 0 || num_char_sent != outpacket_length) {
 		cerr << "sendto() error" << endl;
 		return -1;
 	}
-	
+
+	return 0;
+}
+
+int wait_for_icmp_response(int socket_id, int sequence_number) {
 	fd_set rfds;
 	FD_ZERO(&rfds);
 	FD_SET(socket_id, &rfds);
@@ -141,12 +150,12 @@ int ping(const string &target) {
 			// Now the ICMP part 
 			icmp *icmp_packet = (icmp*)(packet + ip_header_len); 
 			if (icmp_packet->icmp_type == ICMP_ECHOREPLY) {
-				if (icmp_packet->icmp_seq != sequence_number) {
-					cout << "received sequence #" << icmp_packet->icmp_seq << ", expected " << sequence_number << endl;
+				if (icmp_packet->icmp_id != getpid()) {
+					//cout << "received id " << icmp_packet->icmp_id << endl;
 					continue;
 				}
-				if (icmp_packet->icmp_id != getpid()) {
-					cout << "received id " << icmp_packet->icmp_id << endl;
+				if (icmp_packet->icmp_seq != sequence_number) {
+					cout << "received sequence #" << icmp_packet->icmp_seq << ", expected " << sequence_number << endl;
 					continue;
 				}
 			} else {
@@ -167,10 +176,25 @@ int ping(const string &target) {
 	return -1;
 }
 
-int main(int argc, char **argv) {
+int ping(const string &target) {
+	sockaddr_in to = dns_lookup(target);
+	host_string = reverse_dns_lookup(to);
+	host_string += " (" + get_ip_string(to.sin_addr.s_addr) + ")";
+	printf("PING %s with %d bytes of data\n", host_string.c_str(), ICMP_DATA_LENGTH);
+
+	int socket_id = create_socket();
+	n_short sequence_number = 0;
 	while (true) {
+		sequence_number++;
+		send_icmp_packet(to, socket_id, sequence_number);
+		wait_for_icmp_response(socket_id, sequence_number);
 		sleep(1);
-		ping(argv[1]);
 	}
+
+	return 0;
+}
+
+int main(int argc, char **argv) {
+	ping(argv[1]);
 	return 0;
 }
